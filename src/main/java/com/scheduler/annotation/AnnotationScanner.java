e2e814f7f5fe392336201466f73e7292d9909e33
JBearded <244239255@qq.com>
2016-09-19 16:13:12 +0800

    重构scanner

--- src/main/java/com/scheduler/annotation/AnnotationScanner.java
+++ src/main/java/com/scheduler/annotation/AnnotationScanner.java
@@ -29,40 +29,53 @@ public class AnnotationScanner {
         Set<Class<?>> classSet = PackageScanner.getClasses();
         for(Class<?> clazz : classSet){
             Method[] methods = clazz.getDeclaredMethods();
-            List<TaskConfig.MethodConfig> methodConfigs = new ArrayList<>();
-            for(Method method : methods){
-                Crontab crontab = method.getAnnotation(Crontab.class);
-                if(crontab != null){
-                    String cronExpression = crontab.value();
-                    String[] params = crontab.params();
-                    Class<?>[] paramClasses = method.getParameterTypes();
-                    if(paramClasses.length == params.length){
-                        List<TaskConfig.Param> paramList = new ArrayList<>();
-                        int index = 0;
-                        for(Class<?> paramClass : paramClasses){
-                            String name =  paramClass.getSimpleName().toLowerCase();
-                            ParamType paramType = ParamType.lookup(name);
-                            if(paramType != null){
-                                Object object = paramType.convert(params[index++]);
-                                TaskConfig.Param param = new TaskConfig.Param(paramType.getType(), object);
-                                paramList.add(param);
-                            }else{
-                                logger.error("class {} method {}, @Crontab only support byte,short,int,long,float,double,char,string type", clazz.getName(), method.getName());
-                            }
-                        }
-                        if(index == paramClasses.length){
-                            TaskConfig.MethodConfig methodConfig = new TaskConfig.MethodConfig(method.getName(),cronExpression, paramList);
-                            methodConfigs.add(methodConfig);
-                        }
-                    }
-                }
-            }
+            List<TaskConfig.MethodConfig> methodConfigs = getMethodConfigs(methods);
             if(!methodConfigs.isEmpty()){
                 TaskConfig taskConfig = new TaskConfig(null, clazz.getName(), methodConfigs);
                 result.add(taskConfig);
             }
         }
         return result;
+    }
+
+    private static List<TaskConfig.MethodConfig> getMethodConfigs(Method[] methods){
+        List<TaskConfig.MethodConfig> methodConfigs = new ArrayList<>();
+        for(Method method : methods){
+            Crontab crontab = method.getAnnotation(Crontab.class);
+            if(crontab != null){
+                String cronExpression = crontab.value();
+                String[] params = crontab.params();
+                Class<?>[] paramClasses = method.getParameterTypes();
+                if(paramClasses.length == params.length){
+                    List<TaskConfig.Param> paramList = getMethodParams(paramClasses, params);
+                    if(paramList != null){
+                        TaskConfig.MethodConfig methodConfig = new TaskConfig.MethodConfig(method.getName(),cronExpression, paramList);
+                        methodConfigs.add(methodConfig);
+                    }else{
+                        logger.error("method {}, @Crontab only support byte,short,int,long,float,double,char,string type", method.getName());
+                    }
+                }
+            }
+        }
+        return methodConfigs;
+    }
+
+    private static List<TaskConfig.Param> getMethodParams(Class<?>[] paramClasses, String[] params){
+        List<TaskConfig.Param> paramList = new ArrayList<>();
+        int index = 0;
+        for(Class<?> paramClass : paramClasses){
+            String name =  paramClass.getSimpleName().toLowerCase();
+            ParamType paramType = ParamType.lookup(name);
+            if(paramType != null){
+                Object object = paramType.convert(params[index++]);
+                TaskConfig.Param param = new TaskConfig.Param(paramType.getType(), object);
+                paramList.add(param);
+            }
+        }
+        if(index != paramClasses.length){
+            return null;
+        }
+        return paramList;
     }
 
 
